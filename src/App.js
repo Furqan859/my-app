486e3a356284628ca053deb5829b3fa43fdc15dd
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-20 05:24:01 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import {BrowserRouter , Routes , Route} from 'react-router-dom';
-import Home from './component/Home';
-import About from './component/About';
-import NavBar from './component/NavBar';
-import Notfound from './component/Notfound';
+import './App.css';
+import React,{useState} from 'react';
+
 
 function App() {
+  const [time , setTime]= useState(25);
+  const [interv , setInterv] = useState();
+  const [color ,setColor] = useState();
+
+ const start = () =>{
+   run();
+   setInterv(setInterval(run,1000));
+ };
+ const reset = () => {
+   clearInterval(interv);
+   setTime(25)
+ };
+ var timer = time;
+ const run = ()=>{
+   timer--;
+   if(timer===20){
+     setColor('black');
+   }
+   if(timer===10){
+   setColor('yellow');
+   }
+   if(timer===5){
+     setColor('white')
+ }
+ if(timer>=0){
+   return setTime(timer)
+ }
+}
   return (
-    <div>
-      <BrowserRouter>
-      <NavBar/>
-      <Routes>
-      <Route path="*" element={<Notfound/>}/>
-        <Route path="/" element={<Home/>}/> 
-        <Route path="/about" element={<About/>}/>
-        
-      </Routes>
-      
-      </BrowserRouter>
+    <div className="timer" style={{backgroundColor:color}}>
+    <h3>{time}</h3>
+      <button onClick={()=>start()}>Click</button>
+      <button onClick={()=>reset()}>Reset</button>
     </div>
   );
 }
